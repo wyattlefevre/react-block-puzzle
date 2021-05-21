aa389e163e8001d70ed09454d4089393e8b3c0ea
Wyatt LeFevre <wyatt@neighbor.com>
2021-05-21 11:00:12 -0600

    added win condition

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,13 +6,16 @@ import Level from '../levels/one';
 import { useState } from 'react';
 
 const Game = () => {
-  const scale = 20;
+  const scale = 10;
   const roomHeight = Level.roomHeight;
   const roomWidth = Level.roomWidth;
+  const roomX = roomWidth;
+  const roomY = roomHeight;
   const [boxes, setBoxes] = useState(Level.boxes);
   const [currentStartPos, setCurrentStartPos] = useState({ x: null, y: null });
   const [collidingBoxID, setCollidingBoxID] = useState(null);
   const [controlPos, setControlPos] = useState(null);
+  const [gameWon, setGameWon] = useState(false);
 
   const isCollision = (selectedBox, boxes) => {
     let collision = false;
@@ -48,6 +51,19 @@ const Game = () => {
     });
   };
 
+  const checkWinCondition = () => {
+    let won = true;
+    boxes.forEach(b => {
+      if (b.x < roomX ||
+        b.y < roomY ||
+        b.x + b.width > roomX + roomWidth ||
+        b.y + b.height > roomY + roomHeight
+      )
+        won = false;
+    });
+    setGameWon(won);
+  };
+
   const startDrag = (selectedBox) => {
     console.log('setting', selectedBox);
     setCurrentStartPos({ x: selectedBox.x, y: selectedBox.y });
@@ -75,31 +91,37 @@ const Game = () => {
     } else {
       setControlPos(null);
     }
+    checkWinCondition();
   };
 
   return (
     <div>
-      <div style={{ position: 'absolute', top: roomHeight * scale, left: roomWidth * scale }}>
-        <Room height={roomHeight} width={roomWidth} color='grey' scale={scale} />
-      </div>
-      {boxes.map((box) =>
-        <div key={box.id} style={{ position: 'absolute' }}>
-          <Box
-            id={box.id}
-            x={box.x}
-            y={box.y}
-            height={box.height}
-            width={box.width}
-            color={box.collision ? 'red' : box.color}
-            scale={scale}
-            updatePosition={updateBoxPosition}
-            startDrag={startDrag}
-            stopDrag={stopDrag}
-            collision={box.collision}
-            setPosition={collidingBoxID === box.id ? controlPos : null} />
+      { !gameWon ?
+        <div>
+          <div style={{ position: 'absolute', top: roomY * scale, left: roomX * scale }}>
+            <Room height={roomHeight} width={roomWidth} color='grey' scale={scale} />
+          </div>
+          {boxes.map((box) =>
+            <div key={box.id} style={{ position: 'absolute' }}>
+              <Box
+                id={box.id}
+                x={box.x}
+                y={box.y}
+                height={box.height}
+                width={box.width}
+                color={box.collision ? 'red' : box.color}
+                scale={scale}
+                updatePosition={updateBoxPosition}
+                startDrag={startDrag}
+                stopDrag={stopDrag}
+                collision={box.collision}
+                setPosition={collidingBoxID === box.id ? controlPos : null} />
+            </div>
+          )}
         </div>
-      )}
-      {/* <p>{currentStartPos.x}, {currentStartPos.y}</p> */}
+        :
+        <h1>You win!</h1>
+      }
     </div >
   );
 };
